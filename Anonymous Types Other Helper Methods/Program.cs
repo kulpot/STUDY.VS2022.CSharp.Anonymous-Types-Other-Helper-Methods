d066ef1fccbf0b81f530fbcb5cfd7c92249bfe23
Kulpot <kulpotwahu@gmail.com>
2023-08-16 18:55:03 +0800

    Add project files.

---
+++ Anonymous Types Other Helper Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=ozzx01HbJCI&list=PL259BC2977085CF37&index=3
+//Anonymous Types -- usefull, very convenient
+
+static class MainClass
+{
+    static void Main()
+    {
+        var instance = new { FirstName = "Kulpot", LastName = "King" };
+        var instance2 = new { FirstName = "Kulpot", LastName = "King" };
+        var instance3 = new { FirstName = "Billy", LastName = "King" };
+        Console.WriteLine(instance);
+        Console.WriteLine(instance.GetType());
+        Console.WriteLine(instance.GetHashCode());
+        Console.WriteLine(instance2.GetHashCode());
+        Console.WriteLine(instance3.GetHashCode());
+        Console.WriteLine(instance.Equals(instance2));
+        Console.WriteLine(instance.Equals(instance3));
+
+
+    }
+}
